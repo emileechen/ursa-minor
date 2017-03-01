5a16dffe448d1be61f28f0112e0511aa11cf539f
Emilee Chen <emilee.ty.chen@gmail.com>
2017-02-28 19:00:11 -0800

    upper div art minor reqs; lettergradetogpa fn

--- minors.js
+++ minors.js
@@ -1,21 +1,48 @@
 
 
-
 minors = {
 	"ART": {
 		"full": "Art Practice",
 		"reqs": [
 			{	"name": "Art History (20th Century)",
-				"courses": ["HA 192G"],
+				"courses": ["HISTART 192G"],
 				"num": 1
 			},
 			{	"name": "Upper Division",
-				"courses": ["ART 117", "ART 118", "ART 119", "ART 123"],
+				"courses": ["ART 102", "ART 117", "ART 118", "ART 119",
+							"ART 120", "ART 122", "ART 123", "ART 124",
+							"ART 132", "ART 137", "ART 138", 
+							"ART 141", "ART 142",
+							"ART 160", "ART 162", "ART 164", "ART 165",
+							"ART 171", "ART 173", "ART 174", "ART 175", "ART 178", "ART C179", "ART 179",
+							"ART 180",
+							"FILM 180", "FILM 181", "FILM 185", "FILM 186",
+							"TDPS 173", "TDPS 174", "TDPS 175",
+							"VISSTD 180A", "VISSTD 180B", "VISSTD 181", "VISSTD 185", "VISSTD C185A", "VISSTD 187A", "VISST 187B",
+							"NWMEDIA 190"],
 				"num": 4
 			},
 			{	"name": "Lower Division",
-				"courses": ["ART 8", "ART 12", "ART 13", "ART 14", "ART 16", "ART 21", "ART 23AC", "ART 26"],
+				"courses": ["ART 8", "ART 12", "ART 13", "ART 14", "ART 16", "ART 21", "ART W23AC", "ART 26"],
 				"num": 4
+			}
+		]
+	},
+	"COMPSCI": {
+		"full": "Computer Science",
+		"gpa": 2.0,
+		"reqs": [
+			{	"name": "Lower Division",
+				"courses": ["COMPSCI 61A", "COMPSCI 61B", "COMPSCI 61BL", "COMPSCI 61C",
+							"COMPSCI 70"],
+				"num": 5
+			},
+			{	"name": "Upper Division",
+				"courses": ["COMPSCI 160", "COMPSCI 161", "COMPSCI 162", "COMPSCI 164",
+							"COMPSCI 170", "COMPSCI 174",
+							"COMPSCI 184", "COMPSCI 186", "COMPSCI 188", "COMPSCI 189"],
+				"num": 3
+
 			}
 		]
 	}
